31ca1a5d93069e21cc5c930e53051c9172243741
Marcelo Pires <marcpiresrj@gmail.com>
2020-03-02 19:32:27 -0300

    Remove bootstrap script

--- flux-bootstrap.sh
+++
@@ -1,8 +0,0 @@
-export REPOUSER="marcpires"
-
-fluxctl install \
---git-user=${REPOUSER} \
---git-email=marcpiresrj@gmail.com \
---git-url=git@github.com:${REPOUSER}/gitops.git \
---git-path=workloads,namespaces \
---namespace=flux | sudo microk8s.kubectl apply -f -
